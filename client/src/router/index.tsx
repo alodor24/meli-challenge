da0ecf2e8a5e6c4bee0dc610324979263e39fa92
Jose Alejandro <jose-a-mendez@hotmail.com>
2023-03-31 17:40:15 -0300

    Configure routes app

---
+++ client/src/router/index.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+import List from "../pages/List";
+import Detail from "../pages/Detail";
+
+export const router = createBrowserRouter([
+  {
+    index: true,
+    element: <Home />,
+  },
+  {
+    path: "/list",
+    element: <List />,
+  },
+  {
+    path: "/detail",
+    element: <Detail />,
+  },
+]);
