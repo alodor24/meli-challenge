d008d18981e7c3e0b5fc1a2476ad71124043b24a
Jose Alejandro <jose-a-mendez@hotmail.com>
2023-04-02 21:40:19 -0300

    Create footer styles component

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,5 +1,20 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p className="footer-copy">
+          Desarrollado por José A. Méndez |{" "}
+          <a
+            className="footer-link"
+            target="_blank"
+            href="https://github.com/alodor24/meli-challenge"
+          >
+            Github Repository
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
